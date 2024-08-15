5473d5c3a8b7b5dce4e0f42fb8dcd393db66c40b
unknown <nhd7noushadazeez@gmail.com>
2024-08-15 10:14:57 +0530

    food ui assets and dummy list

---
+++ lib/details_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({super.key, required this.food});
+  final Map<String, dynamic> food;
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
